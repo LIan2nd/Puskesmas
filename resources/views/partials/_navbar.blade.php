8898d97f826c319281b7a77024d14c8a69140970
LIan2nd <alfiannurusyaid19@gmail.com>
2023-05-30 15:27:18 +0700

    Update Header & Sidebar, hhe

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo" href="index.html"><img src="{{ asset('admin/assets/images/logo.svg') }}"
+        <a class="navbar-brand brand-logo" href="/"><img src="{{ asset('admin/assets/images/logo.svg') }}"
                 alt="logo" /></a>
-        <a class="navbar-brand brand-logo-mini" href="index.html"><img
-                src="{{ asset('admin/assets/images/logo-mini.svg') }}" alt="logo" /></a>
+        <a class="navbar-brand brand-logo-mini" href="/"><img src="{{ asset('admin/assets/images/logo-mini.svg') }}"
+                alt="logo" /></a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-stretch">
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -28,7 +28,7 @@
                         <span class="availability-status online"></span>
                     </div>
                     <div class="nav-profile-text">
-                        <p class="mb-1 text-black">David Greymaax</p>
+                        <p class="mb-1 text-black">Alfian Nur Usyaid</p>
                     </div>
                 </a>
                 <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
